43198423ca5b85f9fc93190aef7e844aba597a71
Rophy Tsai <rophy@users.noreply.github.com>
2026-07-01 14:20:49 +0000

    feat: main entry point and PgWriter

---
+++ cmd/dblog/main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"context"
+	"database/sql"
+	"flag"
+	"fmt"
+	"log/slog"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+	_ "github.com/godror/godror"
+
+	"github.com/null-ptr-exception/dblog-cdc/internal/chunk"
+	"github.com/null-ptr-exception/dblog-cdc/internal/config"
+	"github.com/null-ptr-exception/dblog-cdc/internal/olr"
+	"github.com/null-ptr-exception/dblog-cdc/internal/progress"
+	"github.com/null-ptr-exception/dblog-cdc/internal/replicator"
+	"github.com/null-ptr-exception/dblog-cdc/internal/writer"
+)
+
+func main() {
+	configPath := flag.String("config", "config.yaml", "path to config file")
+	flag.Parse()
+
+	cfg, err := config.Load(*configPath)
+	if err != nil {
+		slog.Error("load config", "error", err)
+		os.Exit(1)
+	}
+
+	ctx, cancel := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer cancel()
+
+	oracleDB, err := sql.Open("godror", cfg.Source.DSN)
+	if err != nil {
+		slog.Error("connect oracle", "error", err)
+		os.Exit(1)
+	}
+	defer oracleDB.Close()
+
+	ybPool, err := pgxpool.New(ctx, cfg.Target.DSN)
+	if err != nil {
+		slog.Error("connect yugabytedb", "error", err)
+		os.Exit(1)
+	}
+	defer ybPool.Close()
+
+	pgStore := progress.NewPgStore(ybPool, cfg.Progress.Table)
+	if err := pgStore.EnsureTable(ctx); err != nil {
+		slog.Error("ensure progress table", "error", err)
+		os.Exit(1)
+	}
+
+	tableNames := make([]string, len(cfg.Tables))
+	for i, t := range cfg.Tables {
+		tableNames[i] = t.Name
+	}
+
+	for _, tbl := range cfg.Tables {
+		slog.Info("starting replication", "table", tbl.Name)
+
+		cdcClient := olr.NewClient(cfg.CDC.Host, cfg.CDC.Port, "", tableNames)
+		querier := chunk.NewOracleQuerier(oracleDB, "ID")
+		ybWriter := writer.NewPgWriter(ybPool, "ID")
+
+		r := replicator.New(cdcClient, querier, ybWriter, pgStore, tbl)
+		if err := r.Run(ctx); err != nil {
+			slog.Error("replication failed", "table", tbl.Name, "error", err)
+			os.Exit(1)
+		}
+	}
+
+	fmt.Println("replication complete")
+}
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/null-ptr-exception/dblog-cdc
 go 1.25.0
 
 require (
+	github.com/godror/godror v0.51.0
 	github.com/jackc/pgx/v5 v5.10.0
 	google.golang.org/grpc v1.82.0
 	google.golang.org/protobuf v1.36.11
@@ -10,11 +11,15 @@ require (
 )
 
 require (
+	github.com/VictoriaMetrics/easyproto v0.1.4 // indirect
+	github.com/go-logfmt/logfmt v0.6.0 // indirect
+	github.com/godror/knownpb v0.3.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
 	github.com/jackc/puddle/v2 v2.2.2 // indirect
 	github.com/kr/text v0.2.0 // indirect
 	github.com/rogpeppe/go-internal v1.15.0 // indirect
+	golang.org/x/exp v0.0.0-20250506013437-ce4c2cf36ca6 // indirect
 	golang.org/x/net v0.53.0 // indirect
 	golang.org/x/sync v0.20.0 // indirect
 	golang.org/x/sys v0.43.0 // indirect
